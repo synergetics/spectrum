aa3ef7c037a28cbc93c968116726193305553933
ixaxaar <perceptionvoid@gmail.com>
2015-03-15 17:35:13 +0530

    Get cum3est to work :grin:

--- src/conventional/cum3est.py
+++ src/conventional/cum3est.py
@@ -36,36 +36,38 @@ def cum3est(y, maxlag, nsamp, overlap, flag, k1):
 
   y_cum = np.zeros([maxlag-minlag+1,1])
 
-  nd = np.arange(nsamp).T
-  nlags = 1*maxlag + 1
-  zlag = 1 + maxlag
+  ind = np.arange(nsamp).T
+  nlags = 2*maxlag + 1
+  zlag = maxlag
   if flag == 'biased':
     scale = np.ones([nlags, 1])/nsamp
   else:
     lsamp = nsamp - abs(k1)
-    scale = make_arr((range(lsamp-maxlag, lsamp), range(lsamp, lsamp-maxlag,-1)), axis=1)
+    scale = make_arr((range(lsamp-maxlag, lsamp+1), range(lsamp-1, lsamp-maxlag-1, -1)), axis=1).T
     (m2,n2) = scale.shape
     scale = np.ones([m2,n2]) / scale
 
   y = y.ravel(order='F')
   for i in xrange(nrecord):
     x = y[ind]
-    x = x.ravel(order='F') - np.mean(x)
+    x = x - np.mean(x)
     cx = np.conj(x)
     z = x * 0
 
     # create the "IV" matrix: offset for second lag
     if k1 > 0:
-      z[q:nsamp-k1] = x[0:nsamp-k1, :] * cx[k1:nsamp, :]
+      z[0:nsamp-k1] = x[0:nsamp-k1] * cx[k1:nsamp]
     else:
       z[-k1:nsamp] = x[-k1:nsamp] * cx[0:nsamp+k1]
 
-    # compute third-order cumulants
-    y_cum[zlag] = y_cum[zlag] + (z.T * x)
 
-    for k in xrange(maxlag):
-      y_cum[zlag-k] = y_cum[zlag-k] + z[k:nsamp].T * x[0:nsamp-k]
-      y_cum[zlag+k] = y_cum[zlag+k] + z[0:nsamp-k].T * x[k:nsamp]
+    # compute third-order cumulants
+    y_cum[zlag] = y_cum[zlag] + np.dot(z.T, x)
+
+    for k in xrange(1,maxlag+1):
+      y_cum[zlag-k] = y_cum[zlag-k] + np.dot(z[k:nsamp].T, x[0:nsamp-k])
+      y_cum[zlag+k] = y_cum[zlag+k] + np.dot(z[0:nsamp-k].T, x[k:nsamp])
+
 
     ind = ind + int(nadvance)
 
